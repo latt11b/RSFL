00bd4406bc71827e10b17eef7379bd08c8f1d3e9
latt11b <zach22213@yahoo.com>
2017-10-23 07:49:32 -0400

    Delete Soundcloud.js

--- Soundcloud.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" src="scripts/soundcloud.player.api.js"></script>
-<script type="text/javascript">
-     soundcloud.addEventListener('onPlayerReady', function(player, data) {
-       player.api_play();
-     });
-</script>
